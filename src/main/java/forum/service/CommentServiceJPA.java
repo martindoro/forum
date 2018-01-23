08a3e103daa877f4d40b924ec13a8afd32f7d140
student <solar.lukas@gmail.com>
2018-01-23 09:35:19 +0100

    CommentService added

--- src/main/java/forum/service/CommentServiceJPA.java
+++ src/main/java/forum/service/CommentServiceJPA.java
@@ -1,5 +1,31 @@
 package forum.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import forum.entity.Comment;
+
+@Transactional
 public class CommentServiceJPA {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void addComment(Comment comment) {
+		entityManager.persist(comment);
+	}
+
+	public List<Comment> getCommentsUser(String userName) {
+		return entityManager
+				.createQuery("SELECT c FROM Comment c WHERE c.userName = :userName ORDER BY c.createdOn DESC")
+				.setParameter("userName", userName).getResultList();
+	}
+
+	public List<Comment> getCommentsTopic(String topicID) {
+		return entityManager.createQuery("SELECT c FROM Comment c WHERE c.topicID = :topicID ORDER BY c.createdOn ASC")
+				.setParameter("topicID", topicID).getResultList();
+	}
 }
