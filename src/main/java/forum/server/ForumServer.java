abf90eb147b0eff93e5d3b07376a49e715229c7f
student <jakub.magyar@gmail.com>
2018-01-23 09:42:35 +0100

    added JPA services to Server

--- src/main/java/forum/server/ForumServer.java
+++ src/main/java/forum/server/ForumServer.java
@@ -3,6 +3,13 @@ package forum.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import forum.service.CategoryServiceJPA;
+import forum.service.CommentServiceJPA;
+import forum.service.FavoriteServiceJPA;
+import forum.service.TopicServiceJPA;
+import forum.service.UserServiceJPA;
 
 
 
@@ -15,9 +22,26 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 			SpringApplication.run(ForumServer.class, args);
 		}
 
-//		@Bean
-//		/*TODO doplnit JPA services /* 
-//
+		@Bean
+		public CategoryServiceJPA categoryService() {
+			return new CategoryServiceJPA();
+		}
+		@Bean
+		public CommentServiceJPA commentService() {
+			return new CommentServiceJPA();
+		}
+		@Bean
+		public FavoriteServiceJPA favoriteService(){
+			return new FavoriteServiceJPA();
+		}
+		@Bean
+		public TopicServiceJPA topicService() {
+			return new TopicServiceJPA();
+		}
+		@Bean
+		public UserServiceJPA userService() {
+			return new UserServiceJPA();
+		}
 	}
 
 
