52b83026b917d9f3db864ad6588b111afb8b890d
student <matuskollar17@gmail.com>
2018-01-23 09:41:44 +0100

    commentController request mapping update

--- src/main/java/forum/server/controller/CommentController.java
+++ src/main/java/forum/server/controller/CommentController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class CommentController {
 
-	@RequestMapping("/")
+	@RequestMapping("/comment")
 	public String user(Model model) {
 		return "index";
 
