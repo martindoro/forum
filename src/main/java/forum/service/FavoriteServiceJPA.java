f1956fd4c5df330ed0f8cd3ecc00811b12c199a0
student <martin.doro@gmail.com>
2018-01-23 09:38:17 +0100

    add favoriteservice

--- src/main/java/forum/service/FavoriteServiceJPA.java
+++ src/main/java/forum/service/FavoriteServiceJPA.java
@@ -1,5 +1,44 @@
 package forum.service;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+import forum.entity.Favorite;
+
 public class FavoriteServiceJPA {
 
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	public void setFavorite(Favorite favorite) {
+		try {
+			entityManager.createQuery("SELECT f FROM Favorite f WHERE f.userName = :userName AND f.commentId = :commentId")
+					.setParameter("userName", favorite.getUserName()).setParameter("commentId", favorite.getCommentId())
+					.getSingleResult();
+			entityManager.createQuery("DELETE FROM Favorite f WHERE f.userName = :username AND f.game = :game")
+					.setParameter("userName", favorite.getUserName()).setParameter("commentId", favorite.getCommentId())
+					.executeUpdate();
+		} catch (NoResultException e) {
+			entityManager.persist(favorite);
+		}
+	}
+
+	public List<Favorite> getFavorite(String userName) {
+		return entityManager.createQuery("SELECT f FROM Favorite f WHERE f.userName = :userName")
+				.setParameter("username", userName).getResultList();
+	}
+
+	public boolean isFavorite(Favorite favorite) {
+		try {
+			entityManager.createQuery("SELECT f FROM Favorite f WHERE f.userName = :userName AND f.commentId = :commentId")
+					.setParameter("userName", favorite.getUserName()).setParameter("commentId", favorite.getCommentId())
+					.getSingleResult();
+			return true;
+		} catch (NoResultException e) {
+			return false;
+		}
+	}
 }
