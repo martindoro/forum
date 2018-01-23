70fd3ad114cf637c48bb35b03a32d3fafd7587e6
student <jakub.magyar@gmail.com>
2018-01-23 11:00:41 +0100

    Edit forum controller

--- src/main/java/forum/server/controller/ForumController.java
+++ src/main/java/forum/server/controller/ForumController.java
@@ -1,16 +1,33 @@
 package forum.server.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import forum.service.CommentServiceJPA;
+
 @Controller
 public class ForumController {
+	@Autowired
+	private CommentServiceJPA commentService;
+	
 
 	@RequestMapping("/")
 	public String user(Model model) {
+		fillModel(model);
 		return "index";
 
+	}
+	
+	@RequestMapping("/register")
+	public String register(Model model) {
+		return "register";
+	}
+	
+	
+	private void fillModel(Model model) {
+		model.addAttribute("getComments", commentService.getCommentsTopic(0));
 	}
 	
 	
