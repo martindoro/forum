3099f4e664d3651a8c3478121c97e50cf879adb0
student <solar.lukas@gmail.com>
2018-02-02 09:46:08 +0100

    js files added

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -1,22 +1,7 @@
-
-//Function To Display Popup
-function div_show() {
-document.getElementById('abc').style.display = "block";
-}
-//Function to Hide Popup
-function div_hide(){
-document.getElementById('abc').style.display = "none";
-}
-
-var swear_words_arr=new Array("bad","evil","freak", "kurva");
-var regex = new RegExp('\\b(' + swear_words_arr.join('|') + ')\\b', 'i' );
-
-function validate_user_text() {
-    var text = $('#textarea1').text();
-
-    if(regex.test(text)) {
-        alert("Please refrain from using offensive words"); /* + alert_text */
-        $('#textarea1').focus();
-        return false;
-    }
+function reveal(id) {
+	if (document.getElementById(id).style.display == 'none') {
+		document.getElementById(id).style.display = 'inline';
+	} else {
+		document.getElementById(id).style.display = 'none';
+	}
 }
