fc4f986798b8c849d489192797ed25304c5fd094
student <jakub.magyar@gmail.com>
2018-02-02 12:12:24 +0100

    Bad words filter on comment and reply

--- src/main/resources/static/js/reply.js
+++ src/main/resources/static/js/reply.js
@@ -5,3 +5,14 @@ function reveal(id) {
 		document.getElementById(id).style.display = 'none';
 	}
 }
+
+function profanityCheck() {
+	var profanities = new Array("lukas", "mato", "jakub", "matus");
+	var text =  document.getElementById('textarea1').value; 
+	    for (var i = 0; i < profanities.length; i++) {    	
+	        if(text.toLowerCase().includes(profanities[i].toLowerCase())){
+	        	text = text.toLowerCase().replace(profanities[i].toLowerCase(), "****");
+	        }
+	    }
+	    $("#textarea1").val(text);
+	}
