dd8cf3e706f4119f457daf8388f8b12fe8a4f524
martinD <martin.doro@gmail.com>
2018-02-12 15:16:36 +0100

    favorite tests

--- src/test/java/forum/service/FavoriteServiceJPATest.java
+++ src/test/java/forum/service/FavoriteServiceJPATest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.fail;
 
 import javax.persistence.EntityManager;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,6 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import forum.entity.Favorite;
-import forum.server.ForumServer;
 import forum.server.ForumServerForTest;
 import forum.service.interfaces.FavoriteService;
 
@@ -30,11 +30,23 @@ public class FavoriteServiceJPATest {
 	@Autowired
 	private FavoriteService favoriteService;
 	
+	@Before
+	public void setup() {
+	Favorite f1 = new Favorite("aaa", 15, 1);
+	Favorite f2 = new Favorite("bbb", 16, 0);
+	Favorite f3 = new Favorite("ccc", 17, -1);
+	Favorite f4 = new Favorite("ddd", 18, 1);
+	Favorite f5 = new Favorite("eee", 19, 0);
+	favoriteService.setFavorite(f1);
+	favoriteService.setFavorite(f2);
+	favoriteService.setFavorite(f3);
+	favoriteService.setFavorite(f4);
+	favoriteService.setFavorite(f5);
+	}
+	
 	@Test
 	public void testSetFavorite() {
-		Favorite fav = new Favorite("forumUser", 15);
-		favoriteService.setFavorite(fav);
-		assertEquals(fav, favoriteService.getFavorite("forumUser", 15));
+		assertEquals(15, favoriteService.getFavorite("forumUser", 15).getCommentId());
 	}
 
 	@Test
