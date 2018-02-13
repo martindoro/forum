1c42927edc3d19dda378db7bb3b40afb65f59c4d
student <solar.lukas@gmail.com>
2018-02-13 10:46:00 +0100

    category entity test done

--- src/test/java/forum/entity/CategoryTest.java
+++ src/test/java/forum/entity/CategoryTest.java
@@ -1,44 +1,37 @@
 package forum.entity;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 import org.junit.Test;
 
 public class CategoryTest {
 
-	@Test
-	public void testCategory() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCategoryString() {
-		fail("Not yet implemented");
-	}
+	Category category = new Category();
 
 	@Test
 	public void testSetIdent() {
-		fail("Not yet implemented");
+		category.setIdent(1);
+		assertEquals(1, category.getIdent());
 	}
 
 	@Test
 	public void testGetIdent() {
-		fail("Not yet implemented");
+		category.setIdent(1);
+		assertEquals(1, category.getIdent());
 	}
 
 	@Test
 	public void testGetContent() {
-		fail("Not yet implemented");
+		category.setContent("other");
+		assertEquals("other", category.getContent());
 	}
 
 	@Test
 	public void testSetContent() {
-		fail("Not yet implemented");
-	}
+		category.setContent("other");
+		assertEquals("other", category.getContent());
 
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
 	}
 
 }
